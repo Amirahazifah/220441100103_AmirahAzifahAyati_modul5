6d9974166a4d8fa0775fcd96e133be58312697a0
Amirahazifah <113626717+Amirahazifah@users.noreply.github.com>
2023-11-09 23:48:52 +0700

    Add files via upload

---
+++ DefaultTabelModel.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul5;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author niswah
+ */
+public class DefaultTabelModel extends DefaultTableModel {
+
+    public DefaultTabelModel() {
+    }
+    
+}
